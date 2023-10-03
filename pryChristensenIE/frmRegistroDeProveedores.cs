c199054dacbe2b253c125ec8fbd3e0a6132e29ae
root <mgiraudo@ies21.edu.ar>
2023-10-03 16:04:45 -0300

    gfdfgdfg

--- pryChristensenIE/frmRegistroDeProveedores.cs
+++ pryChristensenIE/frmRegistroDeProveedores.cs
@@ -45,7 +45,7 @@ namespace pryChristensenIE
 
                 separadorTexto = leerLinea.Split(';');
 
-                //lblDatos.Text+=objetoLectorArchivo.ReadLine();
+               
                 dgvDatos.Rows.Add(separadorTexto);
             }
 
